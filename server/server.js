3f0024908e35911c2d520336880d486905dbb18c
Jorge Duarte <sadujorge@gmail.com>
2023-10-04 17:55:45 -0700

    Connected to local db

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const path = require("path");
+const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-}) 
+db.once("open", () => {
+    console.log("Connection to db successful");
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
